b3bdc2807cdc30f1ea414f36e67ae9bcc165b736
Katy Daviscourt and sandro mijatovic <katy+sandro@>
2017-02-13 11:00:30 -0800

    make full webpage functional

--- HomeModule.cs
+++ HomeModule.cs
@@ -1,13 +1,23 @@
-// using Nancy;
-// using System;
-//
-// namespace Contacts
-// {
-//   public class HomeModule : NancyModule
-//   {
-//     public HomeModule()
-//     {
-//       Get["/"] =
-//     }
-//       }
-//     }
+using Nancy;
+using System;
+using System.Collections.Generic;
+using PingPongApp.Objects;
+
+namespace PingPongApp
+{
+  public class HomeModule : NancyModule
+  {
+    public HomeModule()
+    {
+      Get["/"] = _ => {
+        return View["PingPong.cshtml"];
+      };
+
+      Post["/output"] = _ => {
+        PingPong numberList = new PingPong(Request.Form["ceiling-number"]);
+        List<string> finalOutput = numberList.PrintArray();
+        return View["output.cshtml", finalOutput];
+      };
+    }
+  }
+}
